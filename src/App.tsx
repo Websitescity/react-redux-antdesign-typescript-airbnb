10591c6e1d7a9780070e09dccc69578455ff7320
Michal Rafalski <websitescity@gmail.com>
2020-03-22 23:23:13 +0100

    function -> React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
